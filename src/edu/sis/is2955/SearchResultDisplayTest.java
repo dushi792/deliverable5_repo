b92536856ef062770d880113bf92262cab1eeb30
Jun Zhao <dushi792@gmail.com>
2015-07-28 16:05:15 -0400

    Add tests to SearchResultDisplay

--- src/edu/sis/is2955/SearchResultDisplayTest.java
+++ src/edu/sis/is2955/SearchResultDisplayTest.java
@@ -2,18 +2,69 @@ package edu.sis.is2955;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/*
+ * As a user,
+ * I would like to check the search results in different views and sort them by prices,
+ * So that I can quickly locate the result I prefer.
+ * 
+ */
 
 public class SearchResultDisplayTest {
+	private WebDriver driver;
+	  private String baseUrl;
 
-	@Before
-	public void setUp() throws Exception {
-	}
+	  @Before
+	  public void setUp() throws Exception {
+	    driver = new FirefoxDriver();
+	    baseUrl = "http://pittsburgh.craigslist.org";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  }
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	  //@Test
+	  public void testSortPriceFromLowToHigh() throws Exception {
+	    driver.get(baseUrl + "/");
+	    driver.findElement(By.id("query")).sendKeys("apt");
+	    driver.findElement(By.id("query")).sendKeys(Keys.ENTER);
+	    driver.findElement(By.linkText("$$$")).click();
+	    String[] pricestr1 = driver.findElement(By.xpath("//p[5]/a/span")).getText().split("[\\D]+");
+	    String[] pricestr2 = driver.findElement(By.xpath("//p[16]/a/span")).getText().split("[\\D]+");
+	    int price1 = Integer.parseInt(pricestr1[1]);
+	    int price2 = Integer.parseInt(pricestr2[1]);
+	    assertTrue(price1 < price2 || price1 == price2);
+	  }
+	  
+	  @Test
+	  public void testChangetoThumbView() throws Exception {
+	    driver.get(baseUrl + "/");
+	    driver.findElement(By.id("query")).sendKeys("apt");
+	    driver.findElement(By.id("query")).sendKeys(Keys.ENTER);
+	    assertNotNull(driver.findElement(By.cssSelector("div > img")).getText());
+	    
+	    driver.findElement(By.id("picview")).click();
+	    assertNotNull(driver.findElement(By.cssSelector("img.thumb")).getText());
+	    assertNull(driver.findElement(By.cssSelector("div > img")).getAttribute("value"));
+	  }
 
+	  @Test
+	  public void testChangetoListView() throws Exception {
+	    driver.get(baseUrl + "/");
+	    driver.findElement(By.id("query")).sendKeys("apt");
+	    driver.findElement(By.id("query")).sendKeys(Keys.ENTER);
+	    assertNotNull(driver.findElement(By.cssSelector("div > img")).getText());
+	    
+	    driver.findElement(By.id("listview")).click();
+	    assertNull(driver.findElement(By.cssSelector("div > img")).getAttribute("value"));	    
+	  }
+
+	  @After
+	  public void tearDown() throws Exception {
+	    driver.quit();
+	  }
 }
