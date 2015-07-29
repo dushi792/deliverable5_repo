19197c1a2b6df74b5fe78eb0d5701274641bc5c1
Jun Zhao <dushi792@gmail.com>
2015-07-28 20:20:34 -0400

    Add commits

--- src/edu/sis/is2955/SaveAndManageSearchTest.java
+++ src/edu/sis/is2955/SaveAndManageSearchTest.java
@@ -12,43 +12,67 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/*
+ * As a user,
+ * I would like to save and manage my search histories,
+ * So that I can get the latest search update.
+ * 
+ */
+
 public class SaveAndManageSearchTest {
 	private WebDriver driver;
 	private String baseUrl;
 	
+	// Start at the main page for craigslist and log in account
 	@Before
 	public void setUp() throws Exception {
 		driver = new FirefoxDriver();
 		baseUrl = "http://pittsburgh.craigslist.org";
+		driver.get(baseUrl + "/");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("(//a[contains(text(),'my account')])[2]")).click();
+	    driver.findElement(By.id("inputEmailHandle")).sendKeys("dushi792@gmail.com");
+	    driver.findElement(By.id("inputPassword")).sendKeys("Dushi792");
+	    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
 	}
 	
+	// Given that I am in the main page
+	// When I search for something and click "save search" button
+	// Then I should get the search key words saved and jump to saved searches page
 	@Test
 	public void testSaveSearch() {
 		try {
+			// Get to main page
 			driver.get(baseUrl + "/");
-		    driver.findElement(By.cssSelector("a.swp > span.txt")).click();
+
+			// Search for something
+			driver.findElement(By.cssSelector("a.swp > span.txt")).click();
+			
+			// Click "save search" button
 		    driver.findElement(By.linkText("save search")).click();
-		    driver.findElement(By.id("inputEmailHandle")).sendKeys("dushi792@gmail.com");
-		    driver.findElement(By.id("inputPassword")).sendKeys("Dushi792");
-		    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
-		    //driver.findElement(By.className("pickbutton")).click();
+		    
+		    // Check if there's a message tells search was saved
 		    assertEquals("search was saved.", driver.findElement(By.id("alert")).getText());
+		    
+		    // Check if page changed to account page
+		    String title = driver.getTitle();
+		    assertEquals("craigslist account", title);
 		} catch (NoSuchElementException nseex) {
 			fail();
 		}
 	}
 	
+	// Given that I am in the account page
+	// When I click the off button
+	// Then I should get the button change to ons
 	@Test
 	public void testTurnOnAlert() {
 		try {
-			driver.get(baseUrl + "/");
-		    driver.findElement(By.xpath("(//a[contains(text(),'my account')])[2]")).click();
-		    driver.findElement(By.id("inputEmailHandle")).sendKeys("dushi792@gmail.com");
-		    driver.findElement(By.id("inputPassword")).sendKeys("Dushi792");
-		    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+			// Check the button is off at first
 		    driver.findElement(By.linkText("searches")).click();
 		    assertEquals("off", driver.findElement(By.cssSelector("label")).getText());
+		    
+		    // Click the button and see if it changes to on
 		    driver.findElement(By.name("subNotify")).click();
 		    assertEquals("on", driver.findElement(By.cssSelector("label > b")).getText());
 		} catch (NoSuchElementException nseex) {
@@ -56,17 +80,20 @@ public class SaveAndManageSearchTest {
 		}
 	}
 	
+	// Given that I am in the account search page
+	// When I click the "hits" button
+	// Then the results should be sorted
 	@Test
 	public void testSortSavedSearches() {
 		try {
-			driver.get(baseUrl + "/");
-		    driver.findElement(By.xpath("(//a[contains(text(),'my account')])[2]")).click();
-		    driver.findElement(By.id("inputEmailHandle")).sendKeys("dushi792@gmail.com");
-		    driver.findElement(By.id("inputPassword")).sendKeys("Dushi792");
-		    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+			// Click "searches" to change to accout search page
 		    driver.findElement(By.linkText("searches")).click();
+		    
+		    // Click twice to sort hits number from high to low
 		    driver.findElement(By.xpath("//table[@id='savedsearchlist']/thead/tr/th[5]")).click();
 		    driver.findElement(By.xpath("//table[@id='savedsearchlist']/thead/tr/th[5]")).click();
+		    
+		    // Compare two hits number, the first should be larger than the next
 		    int hits1 = Integer.parseInt(driver.findElement(By.id("2541120")).getText());
 		    int hits2 = Integer.parseInt(driver.findElement(By.id("2649915")).getText());
 		    assertTrue(hits1 > hits2);
@@ -75,16 +102,19 @@ public class SaveAndManageSearchTest {
 		}
 	}
 	
+	// Given that I am in the account search page
+	// When I click the "delete" button
+	// Then the results should be deleted
 	@Test
 	public void testDeleteSavedSearches() {
 		try {
-			driver.get(baseUrl + "/");
-		    driver.findElement(By.xpath("(//a[contains(text(),'my account')])[2]")).click();
-		    driver.findElement(By.id("inputEmailHandle")).sendKeys("dushi792@gmail.com");
-		    driver.findElement(By.id("inputPassword")).sendKeys("Dushi792");
-		    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+			// Click "searches" to change to accout search page
 		    driver.findElement(By.linkText("searches")).click();
+		    
+		    // Click "delete" button
 		    driver.findElement(By.xpath("//a[2]/button")).click();
+		    
+		    // Message shows "saved search was delete"
 		    assertEquals("saved search was deleted.", driver.findElement(By.id("alert")).getText());
 		} catch (NoSuchElementException nseex) {
 			fail();
--- src/edu/sis/is2955/SearchResultDisplayTest.java
+++ src/edu/sis/is2955/SearchResultDisplayTest.java
@@ -20,6 +20,7 @@ public class SearchResultDisplayTest {
 	private WebDriver driver;
 	private String baseUrl;
 	
+	// Start at the main page for craigslist for each test
 	@Before
 	public void setUp() throws Exception {
 		driver = new FirefoxDriver();
@@ -27,48 +28,77 @@ public class SearchResultDisplayTest {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 	
+	// Given that I am in the main page
+	// When I search for something and click "$$$" button
+	// Then I should get the search results sorted from price low to high
 	@Test
 	public void testSortPriceFromLowToHigh() throws Exception {
 		driver.get(baseUrl + "/");
+		
+		// Search for word "apt"
 		driver.findElement(By.id("query")).sendKeys("apt");
 		driver.findElement(By.id("query")).sendKeys(Keys.ENTER);
+		
+		// Click "$$$" to sort results
 		driver.findElement(By.linkText("$$$")).click();
 		try {
+			// Extract the price part in result and change it from string to integer
 			String[] pricestr1 = driver.findElement(By.xpath("//p[5]/a/span")).getText().split("[\\D]+");
 			String[] pricestr2 = driver.findElement(By.xpath("//p[16]/a/span")).getText().split("[\\D]+");		
 			int price1 = Integer.parseInt(pricestr1[1]);
 			int price2 = Integer.parseInt(pricestr2[1]);
+			
+			// Compare two prices, the price before should be smaller than after one's,
+			// since it sort in ascending order.
 			assertTrue(price1 < price2 || price1 == price2);
 		} catch (NoSuchElementException nseex) {
 			fail();
 		}
 	}
 	
+	// Given that I am in the main page
+	// When I search for something and click "thumb" button
+	// Then I should get the search results from gallery view to thumb view
 	@Test
 	public void testChangetoThumbView() throws Exception {
 		driver.get(baseUrl + "/");
+		
+		// Search for word "apt"
 		driver.findElement(By.id("query")).sendKeys("apt");
 		driver.findElement(By.id("query")).sendKeys(Keys.ENTER);
 		try {
+			// At default view, its element with css "div > img" should not be null
 			assertNotNull(driver.findElement(By.cssSelector("div > img")).getText());
 			
+			// Change to thumb view
 			driver.findElement(By.id("picview")).click();
+			
+			// The element with css "img.thumb" should not be null
 			assertNotNull(driver.findElement(By.cssSelector("img.thumb")).getText());
+			// Meanwhile, the element with previous css "div > img" should have no vaule
 			assertNull(driver.findElement(By.cssSelector("div > img")).getAttribute("value"));
 		} catch (NoSuchElementException nseex) {
 			fail();
 		}
 	}
 	
+	// Given that I am in the main page
+	// When I search for something and click "list" button
+	// Then I should get the search results from gallery view to list view
 	@Test
 	public void testChangetoListView() throws Exception {
 		driver.get(baseUrl + "/");
+		
+		// Search for word "apt"
 		driver.findElement(By.id("query")).sendKeys("apt");
 		driver.findElement(By.id("query")).sendKeys(Keys.ENTER);
 		try {
+			// At default view, its element with css "div > img" should not be null
 			assertNotNull(driver.findElement(By.cssSelector("div > img")).getText());
 			
+			// Change to list view
 			driver.findElement(By.id("listview")).click();
+			// There should only have words, so element with css "div > img" should have no vaule
 			assertNull(driver.findElement(By.cssSelector("div > img")).getAttribute("value"));
 		} catch (NoSuchElementException nseex) {
 			fail();
