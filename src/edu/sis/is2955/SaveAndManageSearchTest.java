e28eef29788628529910257fe3c6441f03947e3c
Jun Zhao <dushi792@gmail.com>
2015-07-29 10:16:53 -0400

    improve identation

--- src/edu/sis/is2955/SaveAndManageSearchTest.java
+++ src/edu/sis/is2955/SaveAndManageSearchTest.java
@@ -26,11 +26,11 @@ public class SaveAndManageSearchTest {
 	// Start at the main page for craigslist and log in account
 	@Before
 	public void setUp() throws Exception {
-		driver = new FirefoxDriver();
-		baseUrl = "http://pittsburgh.craigslist.org";
-		driver.get(baseUrl + "/");
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("(//a[contains(text(),'my account')])[2]")).click();
+	    driver = new FirefoxDriver();
+	    baseUrl = "http://pittsburgh.craigslist.org";
+	    driver.get(baseUrl + "/");
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    driver.findElement(By.xpath("(//a[contains(text(),'my account')])[2]")).click();
 	    driver.findElement(By.id("inputEmailHandle")).sendKeys("dushi792@gmail.com");
 	    driver.findElement(By.id("inputPassword")).sendKeys("Dushi792");
 	    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
@@ -41,25 +41,25 @@ public class SaveAndManageSearchTest {
 	// Then I should get the search key words saved and jump to saved searches page
 	@Test
 	public void testSaveSearch() {
-		try {
-			// Get to main page
-			driver.get(baseUrl + "/");
+	    try {
+	    	// Get to main page
+	    	driver.get(baseUrl + "/");
 
-			// Search for something
-			driver.findElement(By.cssSelector("a.swp > span.txt")).click();
-			
-			// Click "save search" button
-		    driver.findElement(By.linkText("save search")).click();
-		    
-		    // Check if there's a message tells search was saved
-		    assertEquals("search was saved.", driver.findElement(By.id("alert")).getText());
-		    
-		    // Check if page changed to account page
-		    String title = driver.getTitle();
-		    assertEquals("craigslist account", title);
-		} catch (NoSuchElementException nseex) {
-			fail();
-		}
+	    	// Search for something
+	    	driver.findElement(By.cssSelector("a.swp > span.txt")).click();
+	    	
+	    	// Click "save search" button
+	        driver.findElement(By.linkText("save search")).click();
+	        
+	        // Check if there's a message tells search was saved
+	        assertEquals("search was saved.", driver.findElement(By.id("alert")).getText());
+	        
+	        // Check if page changed to account page
+	        String title = driver.getTitle();
+	        assertEquals("craigslist account", title);
+	    } catch (NoSuchElementException nseex) {
+	    	fail();
+	    }
 	}
 	
 	// Given that I am in the account page
@@ -67,17 +67,17 @@ public class SaveAndManageSearchTest {
 	// Then I should get the button change to ons
 	@Test
 	public void testTurnOnAlert() {
-		try {
-			// Check the button is off at first
-		    driver.findElement(By.linkText("searches")).click();
-		    assertEquals("off", driver.findElement(By.cssSelector("label")).getText());
-		    
-		    // Click the button and see if it changes to on
-		    driver.findElement(By.name("subNotify")).click();
-		    assertEquals("on", driver.findElement(By.cssSelector("label > b")).getText());
-		} catch (NoSuchElementException nseex) {
-			fail();
-		}
+	    try {
+	    	// Check the button is off at first
+	        driver.findElement(By.linkText("searches")).click();
+	        assertEquals("off", driver.findElement(By.cssSelector("label")).getText());
+	        
+	        // Click the button and see if it changes to on
+	        driver.findElement(By.name("subNotify")).click();
+	        assertEquals("on", driver.findElement(By.cssSelector("label > b")).getText());
+	    } catch (NoSuchElementException nseex) {
+	    	fail();
+	    }
 	}
 	
 	// Given that I am in the account search page
@@ -85,21 +85,21 @@ public class SaveAndManageSearchTest {
 	// Then the results should be sorted
 	@Test
 	public void testSortSavedSearches() {
-		try {
-			// Click "searches" to change to accout search page
-		    driver.findElement(By.linkText("searches")).click();
-		    
-		    // Click twice to sort hits number from high to low
-		    driver.findElement(By.xpath("//table[@id='savedsearchlist']/thead/tr/th[5]")).click();
-		    driver.findElement(By.xpath("//table[@id='savedsearchlist']/thead/tr/th[5]")).click();
-		    
-		    // Compare two hits number, the first should be larger than the next
-		    int hits1 = Integer.parseInt(driver.findElement(By.id("2541120")).getText());
-		    int hits2 = Integer.parseInt(driver.findElement(By.id("2649915")).getText());
-		    assertTrue(hits1 > hits2);
-		} catch (NoSuchElementException nseex) {
-			fail();
-		}
+	    try {
+	    	// Click "searches" to change to accout search page
+	        driver.findElement(By.linkText("searches")).click();
+	        
+	        // Click twice to sort hits number from high to low
+	        driver.findElement(By.xpath("//table[@id='savedsearchlist']/thead/tr/th[5]")).click();
+	        driver.findElement(By.xpath("//table[@id='savedsearchlist']/thead/tr/th[5]")).click();
+	        
+	        // Compare two hits number, the first should be larger than the next
+	        int hits1 = Integer.parseInt(driver.findElement(By.id("2541120")).getText());
+	        int hits2 = Integer.parseInt(driver.findElement(By.id("2649915")).getText());
+	        assertTrue(hits1 > hits2);
+	    } catch (NoSuchElementException nseex) {
+	    	fail();
+	    }
 	}
 	
 	// Given that I am in the account search page
@@ -107,22 +107,22 @@ public class SaveAndManageSearchTest {
 	// Then the results should be deleted
 	@Test
 	public void testDeleteSavedSearches() {
-		try {
-			// Click "searches" to change to accout search page
-		    driver.findElement(By.linkText("searches")).click();
-		    
-		    // Click "delete" button
-		    driver.findElement(By.xpath("//a[2]/button")).click();
-		    
-		    // Message shows "saved search was delete"
-		    assertEquals("saved search was deleted.", driver.findElement(By.id("alert")).getText());
-		} catch (NoSuchElementException nseex) {
-			fail();
-		}
+	    try {
+	    	// Click "searches" to change to accout search page
+	        driver.findElement(By.linkText("searches")).click();
+	        
+	        // Click "delete" button
+	        driver.findElement(By.xpath("//a[2]/button")).click();
+	        
+	        // Message shows "saved search was delete"
+	        assertEquals("saved search was deleted.", driver.findElement(By.id("alert")).getText());
+	    } catch (NoSuchElementException nseex) {
+	    	fail();
+	    }
 	}
 	
 	@After
 	public void tearDown() throws Exception {
-		driver.quit();
+	    driver.quit();
 	}
 }
